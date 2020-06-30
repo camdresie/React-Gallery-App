70b801e30e8fe5c9393fb303655ba14370defb0d
Cam <cameron.riemensnider@gmail.com>
2020-06-30 16:12:57 -0700

    Fixed quality issue

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -18,7 +18,7 @@ const PhotoContainer = (props) => {
     const results = props.data;
     let photos;
     if (results.length > 0) {
-        photos = results.map( photo => <Photo url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`} key={photo.id} title={photo.title} /> )
+        photos = results.map( photo => <Photo url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} title={photo.title} /> )
     } else {
         photos = <NotFound />
     }
